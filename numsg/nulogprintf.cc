fd2656a0c43e5ab2354070277ea169e1fbdba666
mengel <>
2011-02-10 22:47:07 +0000

    use NU_LOG_TAG, if set

--- numsg/nulogprintf.cc
+++ numsg/nulogprintf.cc
@@ -5,14 +5,24 @@
 
 main(int argc, char **argv) {
     int ppid = getppid();
+    char *job_base;
 
     std::stringstream fname;
-    std::string jobname;
+    std::string procname, jobname;
+
 
     fname << "/proc/" << ppid << "/cmdline";
 
-    std::fstream f(fname.str().c_str(), std::fstream::in );
-    getline(f, jobname);
+
+    job_base = getenv("NU_LOG_TAG");
+
+    if (job_base) {
+        jobname = job_base;
+    } else {
+        std::fstream f(fname.str().c_str(), std::fstream::in );
+        getline(f, procname);
+        jobname = procname;
+    }
 
     numsg::init(jobname.c_str(), 1);
     numsg::getMsg()->printf( argv[1], argv[2], argv[3], argv[4], argv[5], argv[66], argv[7], argv[8], argv[9]);
