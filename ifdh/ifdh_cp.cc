25f1830328bfcb180dfe34e08fd6ff17f2508a6e
Marc Mengel <mengel@fnal.gov>
2014-01-08 16:27:35 -0600

    -gridftp2 does work on newer ones, even though its not listed in the help ...

--- ifdh/ifdh_cp.cc
+++ ifdh/ifdh_cp.cc
@@ -905,7 +905,7 @@ ifdh::cp( std::vector<std::string> args ) {
      while( keep_going ) {
          stringstream cmd;
 
-         cmd << (use_dd ? "dd bs=512k " : use_cpn ? "cp "  : use_srm ? srm_copy_command  : use_any_gridftp ? "globus-url-copy -nodcau -restart -stall-timeout 14400 " : "false" );
+         cmd << (use_dd ? "dd bs=512k " : use_cpn ? "cp "  : use_srm ? srm_copy_command  : use_any_gridftp ? "globus-url-copy -gridftp2 -nodcau -restart -stall-timeout 14400 " : "false" );
 
          if (use_any_gridftp) {
             if ( use_passive()) {
