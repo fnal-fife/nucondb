46c050c5b94129a4efe0f77c52bb3fa24e27ab18
Lynn Garren <garren@fnal.gov>
2018-02-01 16:17:59 -0600

    c++17 doesn't like throw(whatever) on declarations

--- src/nucondb.h
+++ src/nucondb.h
@@ -34,16 +34,16 @@ public:
      static int _debug;
      // internal in back of getChannelData1
      std::vector<tk> 
-	getTimes(double when,double lookback=0, double lookforw=0) throw(WebAPIException);    // gets list of times in surrounding time, default 1 week
+	getTimes(double when,double lookback=0, double lookforw=0); // throw(WebAPIException);    // gets list of times in surrounding time, default 1 week
 
-     void fetchData(double when) throw(WebAPIException); // gets data for a time in cache
-     void fetchData(long key) throw(WebAPIException);    // gets data for a time in cache
+     void fetchData(double when); // throw(WebAPIException); // gets data for a time in cache
+     void fetchData(long key); // throw(WebAPIException);    // gets data for a time in cache
 
-     Folder( std::string name, std::string url, std::string tag = "") throw(WebAPIException); // bookkeeping...
-     int getNamedChannelData_va(double t, unsigned long chan, std::vector<std::string> names,va_list al) throw(WebAPIException); // fetches data 
-     int getNamedChannelData(double t, unsigned long int chan, std::string names,...) throw(WebAPIException); // fetches data 
-     int getChannelData(double t, unsigned long chan, ...) throw(WebAPIException); // fetches data 
-     long int getKey(double) throw(WebAPIException);
+     Folder( std::string name, std::string url, std::string tag = ""); // throw(WebAPIException); // bookkeeping...
+     int getNamedChannelData_va(double t, unsigned long chan, std::vector<std::string> names,va_list al); // throw(WebAPIException); // fetches data 
+     int getNamedChannelData(double t, unsigned long int chan, std::string names,...); // throw(WebAPIException); // fetches data 
+     int getChannelData(double t, unsigned long chan, ...); // throw(WebAPIException); // fetches data 
+     long int getKey(double); // throw(WebAPIException);
      double getCacheStartTime(){ return _cache_start; };
      double getCacheEndTime(){ return _cache_end; };
 };
